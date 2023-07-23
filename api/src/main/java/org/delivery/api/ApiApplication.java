590780aff569e9289055ffba58df0889e7b04d44
gustjr7297 <gustjr7297@naver.com>
2023-07-23 22:28:32 +0900

    kotlin 적용

--- api/src/main/java/org/delivery/api/ApiApplication.java
+++ api/src/main/java/org/delivery/api/ApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+/*@SpringBootApplication
 public class ApiApplication {
 
     public static void main(String[] args){
         SpringApplication.run(ApiApplication.class, args);
     }
-}
+}*/
